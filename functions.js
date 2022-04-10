c83cdec850e46c01a7f6eee9f0219d5b462e2963
AnthonZh <103381920+AnthonZh@users.noreply.github.com>
2022-04-10 14:57:24 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,8 @@
+//Easter Egg for Rossini
+
+function changeInner(id, val) {
+    const element = document.getElementById(id);
+    element.innerHTML = val;
+}
+
+//Plans to add other functions unlikely - not enough time
