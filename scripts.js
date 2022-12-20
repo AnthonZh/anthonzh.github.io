b1553bcd42b7f0b5487d4e5bc5f2c613f07f6274
AZTSA-3349 <anthony.zhang@stu.fayette.kyschools.us>
2022-12-20 12:46:11 -0500

    Animation for Appearing
    someone figure out appearRightBound() for me please

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
+leftBoundAppeared = false;
+rightBoundAppeared = false;
+function appearLeftBound() {
+    if(!leftBoundAppeared) {
+        let leftBound = document.getElementById('invisible-one');
+        let leftBoundRect = leftBound.getBoundingClientRect();
+        let initLeftBoundY = leftBoundRect.top;
+        if(screenY = initLeftBoundY && !leftBoundAppeared) {
+            document.getElementById('invisible-one').classList.add('animated');
+            console.log('Making leftbound visible');
+            document.getElementById('invisible-one').setAttribute('id', 'visible-one');
+            console.log("Changed invisible id");
+            leftBoundAppeared = true;
+        }
+    }   
+}
+function appearRightBound() {
+    if(!rightBoundAppeared)
+    {
+        let rightBound = document.getElementsByClassName('right-bound-container');
+        let rightBoundRect = rightBound[0].getBoundingClientRect();
+        let initRightBoundY = rightBoundRect.top;
+        if(screenY = initRightBoundY && !rightBoundAppeared) {
+            document.getElementById('invisible-two').classList.add('animated');
+            console.log('Making rightBound visible');
+            document.getElementById('invisible-two').setAttribute('id', 'visible-two');
+            console.log("Changed invisible id");
+            rightBoundAppeared = true;
+        }
+    }
+}
 
+window.onscroll = function() {
+    appearLeftBound();
+    //appearRightBound();
+}
