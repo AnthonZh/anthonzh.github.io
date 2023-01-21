be0c2b41ce7a4a6a1d854499fe9e7101f77de269
AZTSA-3349 <anthonyzh3349@gmail.com>
2023-01-20 22:35:59 -0500

    Update Month Button
    Remove glitchy background color, make the button able to be opened and closed: moved the onclick in the html to js.

--- scheduling-scripts.js
+++ scheduling-scripts.js
@@ -1,3 +1,29 @@
 let initialMonth = "Select Month";
 let initialDay = "Select Day";
 let initialYear = "Select Year";
+
+//The function to show/unshow the month selection button.
+document.getElementById('month').innerText = initialMonth;
+let monthShown = false;
+document.getElementById("month").onclick = show;
+
+function show() {
+    var monthSelection = document.getElementById('month-selections');
+    if(!monthShown)
+    {    
+        monthSelection.style.display = 'block'; 
+
+        let width = document.querySelector('#longest').offsetWidth;
+        monthSelection.style.width = width + width/4;
+
+        monthShown = !monthShown;
+    }
+    else 
+    {
+        monthSelection.style.display = 'none';
+        monthSelection.style.width = 0;
+        monthShown = !monthShown;
+    }
+}
+
+months = document.getElementsByClassName('month-options');
