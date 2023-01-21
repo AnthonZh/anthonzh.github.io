fcf8bd9853210fb1b50bbc5946e130214d2c4e7a
sophiastaples <122235770+sophiastaples@users.noreply.github.com>
2023-01-21 13:39:04 -0500

    Add Day Change Functionality

--- scheduling-scripts.js
+++ scheduling-scripts.js
@@ -1,6 +1,8 @@
 let initialMonth = "Select Month";
 let initialDay = "Select Day";
 let initialYear = "Select Year";
+Date date = new Date();
+
 document.getElementById('day').style.display = 'none';
 
 document.getElementById('month').innerText = initialMonth;
@@ -47,3 +49,61 @@ function clickMonth() {
 
 
 document.getElementById('day').innerText = initialDay;
+
+
+function changeDayButtonUp() {
+    var currDay = document.getElementByID('current-day').parseInt();
+    var change = 0;
+    var max = getMaxDay(document.getElementByID('month').innerText);
+    if(this.id == 'day-lower')
+    {
+        change = 1;
+    }
+    else 
+    {
+        change = -1
+    }
+    if(currDay + change > max) {
+        changeDay(0);   
+    }
+    else if(currDay + change < 0) {
+        changeDay(max);
+    }
+    else {
+        changeDay(currDay + change);
+    }
+}
+
+function changeDay(day) {
+    document.getElementByID('current-day') = day;
+}
+
+function getMaxDay(month) {
+    switch month {
+        case "January":
+        case "March":
+        case "May":
+        case "July":
+        case "August":
+        case "October":
+        case "December":
+            return 31;
+            break;
+        case "April":
+        case "June":
+        case "September":
+        case "November":
+            return 30;
+            break;
+        case "February":
+            var year = date.getFullYear();
+            if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                return 29;
+                break;
+            }
+            
+            return 28;
+            break;
+    }
+    return -1;
+|
