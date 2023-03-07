746a837885e0396ad2363f5fde970da5e92f3424
AZTSA-3349 <anthonyzh3349@gmail.com>
2023-03-06 23:39:58 -0500

    Finished pricing, added an x-button

--- pricing-scripts.js
+++ pricing-scripts.js
@@ -12,6 +12,12 @@ const ids = {
     "kuiper": 'kuiper-info',
 };
 
+for(var x of document.getElementsByClassName('x-button')) {
+    x.onclick = () => {
+        disappearAllButId('');
+    }
+}
+
 var moonContainer = document.getElementById('moon');
 var marsContainer = document.getElementById('mars');
 var gasContainer = document.getElementById('gas-giants');
@@ -19,28 +25,32 @@ var kuiperContainer = document.getElementById('kuiper-belt');
 
 moonContainer.onclick = () => {
     if(appeared['moon'] == false) disappearAllButId(ids['moon']); 
-    appeared['moon'] = appear(ids['moon'], appeared['moon']); 
+    appeared['moon'] = !appeared['moon'];
+    appear(ids['moon'], appeared['moon']); 
 };
 
 marsContainer.onclick = () => {
     if(appeared['mars'] == false) disappearAllButId(ids['mars']); 
-    appeared['mars'] = appear(ids['mars'], appeared['mars']); 
+    appeared['mars'] = !appeared['mars'];
+    appear(ids['mars'], appeared['mars']); 
 };
 
 gasContainer.onclick = () => {
     if(appeared['gas'] == false) disappearAllButId(ids['gas']); 
-    appeared['gas'] = appear(ids['gas'], appeared['gas']); 
+    appeared['gas'] = !appeared['gas'];
+    appear(ids['gas'], appeared['gas']); 
 };
 
 kuiperContainer.onclick = () => {
     if(appeared['kuiper'] == false) disappearAllButId(ids['kuiper']); 
-    appeared['kuiper'] = appear(ids['kuiper'], appeared['kuiper']); 
+    appeared['kuiper'] = !appeared['kuiper'];
+    appear(ids['kuiper'], appeared['kuiper']); 
 };
 
 function appear(id, appeared) {
     let info = document.getElementById(id);
 
-    if(!appeared) {
+    if(appeared) {
         info.classList.remove('disappear');
         info.classList.add('appear'); 
 
@@ -56,14 +66,15 @@ function appear(id, appeared) {
             info.style.opacity = '0';
         }, 1000);
     }
-    return !appeared
+    
 }
 
 function disappearAllButId(id) {
     for(const key in appeared) {
         if(key == id) continue;
         if(appeared[key] == true) {
-            appeared[key] = appear(ids[key], appeared[key]);
+            appeared[key] = !appeared[key];
+            appear(ids[key], appeared[key]);
         }
     }
 }
