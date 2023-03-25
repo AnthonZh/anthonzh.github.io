2210af24f8cd74b4dacc4b1ce8443f9d99c0cf5a
AZTSA-3349 <anthonyzh3349@gmail.com>
2023-03-25 17:20:13 -0400

    asdf

---
+++ trip-scripts.js
@@ -0,0 +1,52 @@
+var leftBoundAppeared = false;
+var rightBoundAppeared = false;
+
+var initFirstY = document.getElementById('invisible-one').getBoundingClientRect().top + window.scrollY - (document.getElementById('invisible-one').getBoundingClientRect().height/2);
+var initSecondY = document.getElementById('invisible-two').getBoundingClientRect().top + window.scrollY - (document.getElementById('invisible-two').getBoundingClientRect().height/2);
+
+var body = document.getElementsByTagName('body')[0];
+
+function appearLeftBound() {
+    if(window.scrollY >= initFirstY) {
+        document.getElementById('invisible-one').classList.add('animated');
+        console.log('Making leftbound visible');
+
+        document.getElementById('invisible-one').setAttribute('id', 'visible-one');
+        console.log("Changed invisible id");
+
+        leftBoundAppeared = true;
+    }   
+}
+
+function appearRightBound() {
+    if(window.scrollY >= initSecondY)
+    {
+        document.getElementById('invisible-two').classList.add('animated');
+        console.log('Making rightBound visible');
+
+        document.getElementById('invisible-two').setAttribute('id', 'visible-two');
+        console.log("Changed invisible id");
+
+        rightBoundAppeared = true;
+    }
+}
+
+function changeBackground() {
+    if(window.scrollY >= document.getElementById('title-container').getBoundingClientRect().height) {
+        if(body.getAttribute('id') == 'background') {
+            body.setAttribute('class', 'forward-background');
+            body.setAttribute('id', 'background-two');
+            console.log("doing something")
+        }
+    }
+}
+
+window.onscroll = function() {
+    if(!leftBoundAppeared) {
+        appearLeftBound();
+    }
+
+    if(!rightBoundAppeared) {
+        appearRightBound();
+    }
+}
