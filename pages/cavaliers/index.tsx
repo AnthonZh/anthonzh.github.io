7cd7c8d3dfa7e36a7f07a88aa3110800c33f1953
AnthonZh <anthonyzh3349@gmail.com>
2025-12-14 20:43:41 -0500

    Fixing cavaliers and local files

--- pages/cavaliers/index.tsx
+++ pages/cavaliers/index.tsx
@@ -1,17 +1,21 @@
-import { GetStaticProps } from "next";
-
 import { title } from "@/components/primitives";
 import Slideshow from "@/components/slideshow";
 import DefaultLayout from "@/layouts/default";
-import fs from "fs";
-import path from "path";
+const images = [
+  "/cavaliers/24CAVALIERS-27-L.jpg",
+  "/cavaliers/2025.8.9_smohlermedia-1205-X3.jpg",
+  "/cavaliers/2025.8.9_smohlermedia-1211-X3.jpg",
+  "/cavaliers/cstevens-8096-M.jpg",
+  "/cavaliers/cstevens-18729-M.jpg",
+  "/cavaliers/Julie Finals-042-M.jpg",
+  "/cavaliers/Julie Finals-111-M.jpg",
+  "/cavaliers/RW-CAV-25-finals-run-147-X3.jpg",
+  "/cavaliers/RW-CAV-25-finals-run-152-X3.jpg",
+  "/cavaliers/SPG00436-X3.jpg",
+  "/cavaliers/SPG00582-M.jpg",
+];
 
-
-type Props = {
-  images: string[];
-};
-
-export default function DocsPage({ images }: Props) {
+export default function DocsPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
@@ -20,7 +24,7 @@ export default function DocsPage({ images }: Props) {
         </div>
       </section>
       <section className="flex flex-col gap-4 py-8 md:py-10">
-        <p>
+        <p className="indent-8">
           In 2024 and 2025, I had the opportunity to march at the Cavaliers Drum
           and Bugle Corps. Being a part of this wonderful organization has
           brought me so much joy and camaderie, as well as some of the best
@@ -28,35 +32,32 @@ export default function DocsPage({ images }: Props) {
           you are curious to learn more, this video below does a good job at at
           explaining what this amazing activity is.
         </p>
-        <div className="flex items-center justify-center gap-8">
+        <div className="flex items-center justify-center gap-8 p-8">
           <iframe
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture; web-share"
-            height="500vw"
+            height="250vw"
             referrerPolicy="strict-origin-when-cross-origin"
             src="https://www.youtube.com/embed/j53QbDBKFB0?si=xy8XBgxWaqXn7WnS"
             title="YouTube video player"
-            width="889vh"
+            width="445vh"
           />
         </div>
-        <Slideshow images={images} interval={4500} />
+        <p className="indent-8">
+          From this experience, I have learned to be a better team member,
+          leader, and worker. I have learned about resilience and pushing
+          through adversity to make something bigger than myself. I have made a
+          point to remember these lessons, and the Cavaliers will always hold
+          hold a special place in my heart. Marching a Drum and Bugle Corps is
+          not easy, no matter where you go. The people at the Cavaliers have
+          supported me not just in the summers I marched with them, but also all
+          my personal endeavors. They are the reason I always push myself to
+          become the best version of myself, no matter the circumstance.
+        </p>
+        <div className="flex flex-col items-center justify-center p-8 gap-4">
+          <Slideshow images={images} interval={4500} />
+          <p>Some cool pictures from the amazing Cavaliers media team.</p>
+        </div>
       </section>
     </DefaultLayout>
   );
 }
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  let images: string[] = [];
-
-  try {
-    const files = fs.readdirSync("/cavaliers/");
-
-    images = files
-      .filter((file) => /\.(jpe?g|png|webp|avif|gif|jpg)$/i.test(file))
-      .sort()
-      .map((file) => `/cavaliers/${file}`);
-  } catch {}
-
-  return {
-    props: { images },
-  };
-};
